4d5ceac0a9ee5b0f5e90662bfd6d467f004d1a76
Jacci19 <jacek205@wp.pl>
2019-01-22 10:57:48 +0100

    04c_Added some nice player click effect

--- core/src/pl/jacci/entities/Player.java
+++ core/src/pl/jacci/entities/Player.java
@@ -1,9 +1,13 @@
 package pl.jacci.entities;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
-public class Player extends Image {
+public class Player extends Image {                                     //image z kolei dziedziczy po actor
     private final static int WIDTH = 77;
     private final static int HEIGHT = 152;
 
@@ -21,5 +25,31 @@ public class Player extends Image {
     }
 
     public void reactOnClick() {
+        //Action testAction = Actions.moveBy(10,10);                            //proste przykładowe akcje
+        //Action testAction = Actions.sizeBy(50,1);
+        //this.addAction(testAction);
+
+        int xMoveAmmount = MathUtils.random(-130, 130);         //losowa liczba z podanego zakresu
+        int yMoveAmmount = 10;
+        float moveActionTime = 0.30f;                           //0.3 sekundy
+        Action moveAction = Actions.sequence(                                                                       //sekwencja akcji
+                Actions.moveBy(xMoveAmmount, yMoveAmmount, moveActionTime, Interpolation.circleOut),
+                Actions.moveBy(-xMoveAmmount, -yMoveAmmount, moveActionTime, Interpolation.circle)
+        );
+
+        int xGrowAmmount = MathUtils.random(-30, 100);
+        int yGrowAmmount = 20;
+        float growActionTime = 0.2f;
+        Action growAction =  Actions.sequence(
+                Actions.sizeBy(xGrowAmmount, yGrowAmmount, growActionTime, Interpolation.circleOut),
+                Actions.sizeBy(-xGrowAmmount, -yGrowAmmount, growActionTime, Interpolation.circle)
+        );
+
+        this.addAction(moveAction);
+        this.addAction(growAction);
+
+        if(this.getHeight() > 170){
+            this.addAction(Actions.rotateBy(MathUtils.randomSign() * 360, 0.4f));
+        }
     }
 }

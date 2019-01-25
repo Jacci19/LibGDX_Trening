56a52df21167dbdf6d2aa5abaa4ebfc897bd31c6
Jacci19 <jacek205@wp.pl>
2019-01-25 10:49:34 +0100

    11b_fixed randomize object spawning

--- core/src/pl/jacci/controllers/FlyingObjectController.java
+++ core/src/pl/jacci/controllers/FlyingObjectController.java
@@ -15,31 +15,38 @@ public class FlyingObjectController {
 
     private void init(final TutorialClickerGame game, final Stage stage) {
         randomizeSpawnTime();
-
         Timer.schedule(new Timer.Task() {
 
             @Override
-            public void run() {                             //będzie to powtarzane co czas spawnTime
-
-                FlyingObject flyingObject = null;
-
-                if(MathUtils.randomBoolean()){
-                    flyingObject = new FlyingObject(FlyingObject.FlyingObjectType.MONEY, game);
-                } else{
-                    flyingObject = new FlyingObject(FlyingObject.FlyingObjectType.PASSIVE, game);
-                }
-
-                stage.addActor(flyingObject);
-                flyingObject.fly();
-
-                randomizeSpawnTime();
+            public void run() {                             //https://youtu.be/hAn4k0kut8U?list=PLFq6ri1W22hwmA0FzkR5zPPOnsimwUc9P&t=1482
+                Timer.schedule(new Timer.Task() {
+                    @Override
+                    public void run() {                             //będzie to powtarzane co czas spawnTime
+                        addRandomFlyingObjectToSage(game, stage);
+                        randomizeSpawnTime();
+                    }
+                }, spawnTime);
             }
-        }, spawnTime, spawnTime);       //jak jest jeden spawnTime to run wykona się tylko raz po upływie tego czasu.
-                                        //drugi spawnTime zapewnia nam, że run będzie się wykonywał w petli po każdym upływie tego czasu
+        }, 1,5);                                      //jak jest jeden spawnTime to run wykona się tylko raz po upływie tego czasu.
+                                                                                //drugi spawnTime zapewnia nam, że run będzie się wykonywał w petli po każdym upływie tego czasu
     }
 
     private void randomizeSpawnTime() {
         spawnTime = MathUtils.random(5, 10);        //przypadkowa liczba z zakresu (5, 10)
+    }
+
+    private void addRandomFlyingObjectToSage(TutorialClickerGame game, Stage stage){
+        FlyingObject flyingObject = null;
+
+        if(MathUtils.randomBoolean()){
+            flyingObject = new FlyingObject(FlyingObject.FlyingObjectType.MONEY, game);
+        } else{
+            flyingObject = new FlyingObject(FlyingObject.FlyingObjectType.PASSIVE, game);
+        }
+
+        stage.addActor(flyingObject);
+        flyingObject.fly();
+
     }
 }
 

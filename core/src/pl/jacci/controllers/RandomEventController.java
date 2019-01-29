c3f4b10314bb1821380a4dd75de9cb283e9ec48f
Jacci19 <jacek205@wp.pl>
2019-01-29 12:02:28 +0100

    16b_added random event dialogs

--- core/src/pl/jacci/controllers/RandomEventController.java
+++ core/src/pl/jacci/controllers/RandomEventController.java
@@ -3,18 +3,22 @@ package pl.jacci.controllers;
 /** co 5 sekund losujemy czy ma wystąpic event, jeśli tak to losujemy jeden z trzech eventów */
 
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
 import pl.jacci.TutorialClickerGame;
+import pl.jacci.ui.BasicDialog;
 
 public class RandomEventController {
 
     private static final int RANDOM_TICK_INTERVAL = 5; // TODO: change after initial implementation to bigger number
 
     private TutorialClickerGame game;
+    private Stage stage;
 
-    public RandomEventController(TutorialClickerGame game){
+    public RandomEventController(TutorialClickerGame game, Stage stage){
         this.game = game;
+        this.stage = stage;
         init();
     }
     private void init(){
@@ -47,16 +51,24 @@ public class RandomEventController {
         }
     }
 
+    private void triggerDialog(String text){
+        BasicDialog basicDialog = new BasicDialog();
+        basicDialog.showDialog(stage, text);
+    }
+
     private void gainPassiveIncome() {
         game.getScoreService().addPassiveIncome();
+        triggerDialog("Yeaah! You gained passive income.");
     }
 
     private void gainMoneyEvent(){
         game.getScoreService().addPoints(123);
+        triggerDialog("Yeaah! You gained passive income.");
     }
 
     private void loseMoneyEvent(){
         game.getScoreService().addPoints(-123);
+        triggerDialog("Pay taxes, owl!");
     }
 
 }
--- core/src/pl/jacci/ui/BasicDialog.java
+++ core/src/pl/jacci/ui/BasicDialog.java
@@ -3,6 +3,7 @@ package pl.jacci.ui;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -35,6 +36,12 @@ public class BasicDialog extends Image {
     }
 
     public void initContent(String text){
+        label.setText(text);
+        this.getStage().addActor(label);
+    }
+
+    public  void showDialog(Stage stage, String text){
+        stage.addActor(this);
         label.setText(text);
         this.getStage().addActor(label);
     }
